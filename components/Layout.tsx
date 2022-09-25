be46e22ff2e0500f3210ae62bbb14543e54f95fa
Minung Han <hmu332233@gmail.com>
2022-09-25 17:27:39 +0900

    기본 레이아웃 추가

---
+++ components/Layout.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Header from 'components/Header';
+import Footer from 'components/Footer';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+function Layout({ children }: Props) {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <main className="container mx-auto grow flex flex-col items-center gap-y-8">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,58 +1,10 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
+
+import Layout from 'components/Layout';
 
 const Home: NextPage = () => {
-  {
-    /* <svg width="199" height="199" viewBox="0 0 199 199" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-  <rect width="199" height="199" rx="22" fill="#080F1A"/>
-</svg>  */
-  }
-  return (
-    <svg
-      width="200"
-      height="200"
-      viewBox="0 0 200 200"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="100%" height="100%" rx="22" fill="#080F1A" />
-      <text
-        x="50%"
-        y="40"
-        fill="white"
-        font-size="24"
-        font-family="Inter"
-        dominant-baseline="middle"
-        text-anchor="middle"
-      >
-        New Year
-      </text>
-      <text
-        x="50%"
-        y="50%"
-        fill="white"
-        font-size="48"
-        font-family="Inter"
-        dominant-baseline="middle"
-        text-anchor="middle"
-      >
-        D-365
-      </text>
-      <text
-        x="50%"
-        y="150"
-        fill="white"
-        font-size="16"
-        font-family="Inter"
-        dominant-baseline="middle"
-        text-anchor="middle"
-      >
-        Days
-      </text>
-    </svg>
-  );
+  return <Layout>테스트입니다.</Layout>;
 };
 
 export default Home;
