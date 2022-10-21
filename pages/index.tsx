41693e498378c8f69f58a447695f7589c262a940
Minung Han <hmu332233@gmail.com>
2022-10-21 18:11:55 +0900

    SvgPreview를 ssr에서도 제외하도록 변경

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react';
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 
+import { Suspense } from 'react';
 import Layout from 'components/Layout';
 // import SvgPreview from 'components/SvgPreview';
 import SvgForm from 'components/SvgForm';
-import dynamic from 'next/dynamic';
-const SvgPreview = dynamic(() => import('components/SvgPreview'));
+const SvgPreview = dynamic(() => import('components/SvgPreview'), {
+  ssr: false,
+});
 
 const defaultValues: SvgData = {
   text: 'New Year',
@@ -21,6 +24,7 @@ const Home: NextPage = () => {
         <br />
         such as your Github, Notion, etc
       </p>
+      {/* TODO: 같은 크기로 스켈레톤 추가해서 로딩 효과 주기 */}
       <SvgPreview data={data} />
       <SvgForm defaultValues={defaultValues} onChange={setData} />
     </Layout>
