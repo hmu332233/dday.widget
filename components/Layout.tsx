8f9dfa9aed392178f291ffd742e6ac2aaf2af208
Minung Han <hmu332233@gmail.com>
2022-10-23 00:55:25 +0900

    meta tag 추가

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import Script from 'next/script';
 
 import React from 'react';
@@ -12,19 +13,35 @@ type Props = {
 function Layout({ children }: Props) {
   return (
     <>
-      <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-XS51JN8JHW"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      <Head>
+        <title>D-Day.widget</title>
+        <meta
+          name="description"
+          content="Add D-Day widget to anywhere, such as your Github Profile, Notion, etc"
+        />
+        <meta name="keywords" content="D-Day,widget,github,notion" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="D-Day.widget" />
+        <meta
+          property="og:description"
+          content="Add D-Day widget to anywhere, such as your Github Profile, Notion, etc"
+        />
+        {/* <meta property="og:image" content=""/> */}
+        <meta property="og:url" content="https://dday-widget.minung.dev" />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-XS51JN8JHW"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
 
-          gtag('config', 'G-XS51JN8JHW');
-        `}
-      </Script>
+            gtag('config', 'G-XS51JN8JHW');
+          `}
+        </Script>
+      </Head>
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="container mx-auto grow flex flex-col items-center gap-y-8">
